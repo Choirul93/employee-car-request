a02a6a7a0eedbebed720145172dcd344853dd11b
choirul arifin <choirularifin1010@gmail.com>
2019-08-05 14:45:24 +0700

    [IMP] improving modules by adding more detail

--- __manifest__.py
+++ __manifest__.py
@@ -3,8 +3,7 @@
     'name': "employee_car_request",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        reuest car and get approval """,
 
     'description': """
         Long description of module's purpose
@@ -16,11 +15,11 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Human Resource',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'hr', 'fleet'],
 
     # always loaded
     'data': [
